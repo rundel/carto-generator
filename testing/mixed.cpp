6de6449b24c2527e3006829035764a775d5d8242
Colin Rundel <rundel@gmail.com>
2011-04-15 20:34:46 -0700

    testing examples fail under boost 1.46.1 should work with svn  > r71306

--- testing/mixed.cpp
+++ testing/mixed.cpp
@@ -9,16 +9,36 @@
 class box {       
     private:
         std::string data_;
+        int x_;
+        double y_;
+        bool z_;
          
     public:
-        box() : data_("test") {}   
+        box()
+          : data_("test"),
+            x_(123),
+            y_(456.7),
+            z_(false) {}   
+        
         std::string data() const { return data_;}
+        int x() const {return x_;}
+        double y() const {return y_;}
+        bool z() const {return z_;}
+        
         void set_data(std::string data) { data_=data;}
+        void set_x(int x) {x_=x;}
+        void set_y(double y) {y_=y;}
+        void set_z(bool z) {z_=z;}
+        
 };
 
 BOOST_FUSION_ADAPT_ADT(
     box,
     (std::string, std::string, obj.data(), obj.set_data(val) )
+    (std::string, std::string, obj.data(), obj.set_data(val) )
+    (int,    int,    obj.x(), obj.set_x(val) )
+    (double, double, obj.y(), obj.set_y(val) )
+    (bool,   bool,   obj.z(), obj.set_z(val) )
 );
 
 int main ( int argc , char** argv) {    
@@ -29,16 +49,22 @@ int main ( int argc , char** argv) {
     std::back_insert_iterator<std::string> sink(css_generated);
 
     std::cout << "data: " << m.data() << "\n";
+    std::cout << "x: " << m.x() << "\n";
+    std::cout << "y: " << m.y() << "\n";
+    std::cout << "z: " << m.z() << "\n";
+    
     std::cout << "\n";
     
         
     bool r=boost::spirit::karma::generate(sink,
         
-        // Both of the following rules produce compiler errors under boost 1.46.1
-        // should work with svn > r71306
-        
-        ( "data: " << boost::spirit::karma::string << "\n"),
-        //( "data: " << +boost::spirit::karma::char_ << "\n"),
+        (
+             ("data(string): " << boost::spirit::karma::string << "\n")
+          << ("data(char): " << +boost::spirit::karma::char_ << "\n")
+          << ("x:" << boost::spirit::karma::int_ << "\n")
+          << ("y:" << boost::spirit::karma::double_ << "\n")
+          << ("z:" << boost::spirit::karma::bool_ << "\n")
+        ),
         m
     );
     
