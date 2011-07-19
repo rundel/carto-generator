483fbe38d153b507dc059f7657e54277cf452c77
Colin Rundel <rundel@gmail.com>
2011-07-19 00:10:02 -0400

    moved prettify function to a header

--- example.cpp
+++ example.cpp
@@ -7,45 +7,8 @@
 
 #include <iostream>
 
+#include "prettify.hpp"
 #include "gen_map.hpp"
-
-std::string prettify(std::string in, std::string indent = "    ") {
-    
-    std::string out;
-    out.reserve(in.size());
-    
-    char cur, prev;
-    bool in_quote = false;
-    int depth = 0;
-    
-    std::string::iterator it;
-    for(it=in.begin(); it<in.end(); ++it ) {
-        
-        cur = *it;
-        
-        if ((cur == '}' || cur == ']') && !in_quote)
-            depth--;
-        
-        if (prev == '\n' && !in_quote) {
-            int j=depth;
-            while(j-- !=0 ) {
-                out.append(indent);
-            }
-        }
-        
-        if ((cur == '{' || cur == '[') && !in_quote)
-            depth++;
-        
-        if (cur == '"' && prev != '\\')
-            in_quote = !in_quote;
-        
-        out.push_back(cur);
-        
-        prev = cur;
-    }
-    
-    return out;
-}
 
 
 int main ( int argc , char** argv) {    
---
+++ include/prettify.hpp
@@ -0,0 +1,38 @@
+std::string prettify(std::string in, std::string indent = "    ") {
+    
+    std::string out;
+    out.reserve(in.size());
+    
+    char cur, prev;
+    bool in_quote = false;
+    int depth = 0;
+    
+    std::string::iterator it;
+    for(it=in.begin(); it<in.end(); ++it ) {
+        
+        cur = *it;
+        
+        if ((cur == '}' || cur == ']') && !in_quote)
+            depth--;
+        
+        if (prev == '\n' && !in_quote) {
+            int j=depth;
+            while(j-- !=0 ) {
+                out.append(indent);
+            }
+        }
+        
+        if ((cur == '{' || cur == '[') && !in_quote)
+            depth++;
+        
+        if (cur == '"' && prev != '\\')
+            in_quote = !in_quote;
+        
+        out.push_back(cur);
+        
+        prev = cur;
+    }
+    
+    return out;
+}
+ 
